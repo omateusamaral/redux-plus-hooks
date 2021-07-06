1c20d1df9936e717ec462e1a10f38f9bf5cf7b2b
mateusamarall <mateus.amaral018@gmail.com>
2021-07-06 08:53:59 -0300

    estilizaçao de reservas

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
-
+import { MdDelete } from 'react-icons/md';
+import './styles.css';
 export default function Books() {
- return (
-   <div>a</div>
+  return (
+    <div>
+      <h1 className="title">Você solicitou 1 reservas</h1>
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="maceio"
+        />
+        <strong>viagem maceio 7 dias</strong>
+        <span>quantidade: 2</span>
+        <button
+          type="button"
+          onClick={() => { }}
+        >
+          <MdDelete size={20} color="#191919" />
+
+        </button>
+      </div>
+      <footer>
+        <button type="button">
+          Solicitar Reservas
+        </button>
+      </footer>
+    </div>
   );
 }
